151f7f7d35068db45a4ee2d171ddf076afbc6ab0
RudeGames <fs_boukhoulda@esi.dz>
2018-12-12 22:15:42 +0100

    Modified Laravel Interface

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'sql7.freemysqlhosting.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'sql7269756'),
+            'username' => env('DB_USERNAME', 'sql7269756'),
+            'password' => env('DB_PASSWORD', 'xLB25CGrFs'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
